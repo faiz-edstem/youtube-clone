d90132bd7de8fcb0a625559765e4604faaa08583
faize.khan <faize.khan@edstem.com>
2021-12-11 11:57:15 +0530

    added basic folder structure and necessary components

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/header/Header.js'
+import Sidebar from './components/sidebar/Sidebar.js'
+import HomeScreen from './screens/homeScreen/HomeScreen'
 
 function App() {
   return (
-    <h1> Youtbue Clone</h1>
+    <>
+    <Header />
+    <div className="app__container">
+      <Sidebar />
+      <Container fluid className="app__main">
+        <HomeScreen />
+      </Container>
+
+    </div>
+    </>
   );
 }
 
